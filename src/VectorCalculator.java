50f35a456c5317962d5f57557949d6c1ac281087
C21Manuel.Riolo <c21manuel.riolo@usafa.edu>
2019-09-10 10:03:06 -0600

    Added unit calculator functionality. Fixed parseNormExpression functionality.

--- src/VectorCalculator.java
+++ src/VectorCalculator.java
@@ -5,11 +5,11 @@ public class VectorCalculator {
 
     public static void main(String[] args) {
         Pattern startTokens = Pattern.compile("exit|<|\\d|\\||dir|unit");
-        System.out.println("Welcome to the Vector Calculator!\n");
+        System.out.println("Welcome to the Vector Calculator!");
         Scanner in = new Scanner(System.in);
         boolean exit = false;
         while(!exit){
-            System.out.println("Enter a vector expression: ");
+            System.out.println("\nEnter vector expression: ");
             String userInput = in.nextLine();
             Scanner userCmd = new Scanner(userInput);
             if (userCmd.hasNext(startTokens)){
@@ -23,16 +23,11 @@ public class VectorCalculator {
                 else if(userCmd.hasNext("\\d")){
                     parseScaleExpression(userCmd);
                 }
+                else if(userCmd.hasNext("unit")){
+                    parseNormalizeExpression(userCmd);
+                }
             }
         }
-
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("Please enter a vector: ");
-//        String userInput = in.nextLine();
-//        Scanner userCmd = new Scanner(userInput);
-//        Vector330Class myVector = Vector330Class.parseVector(userCmd);
-//        System.out.println(myVector.getX());
-//        System.out.println(myVector.getY());
     }
 
     private static void parseExpression(java.util.Scanner s){
@@ -46,9 +41,8 @@ public class VectorCalculator {
     private static void parseNormExpression(java.util.Scanner s){
         s.next("\\|");
         Vector330Class myVector = Vector330Class.parseVector(s);
-        //TODO normalize(); currently does a unit calculation
-        Vector330Class newVector = myVector.normalize();
-        System.out.println("< " + newVector.getX() + ", " + newVector.getY() + " >");
+        double vectorMagnitude = myVector.magnitude();
+        System.out.println("Result is " + vectorMagnitude);
     }
 
     private static void parseScaleExpression(java.util.Scanner s){
@@ -56,14 +50,16 @@ public class VectorCalculator {
         s.next("\\*");
         Vector330Class myVector = Vector330Class.parseVector(s);
         Vector330Class newVector = myVector.scale(scaleValue);
-        System.out.println(newVector.getX());
-        System.out.println(newVector.getY());
+        System.out.println("< " + newVector.getX() + ", " + newVector.getY() + " >");
     }
     private static void parseDirectionExpression(java.util.Scanner s){
 
     }
 
     private static void parseNormalizeExpression(java.util.Scanner s){
-
+        s.next("unit");
+        Vector330Class myVector = Vector330Class.parseVector(s);
+        Vector330Class newVector = myVector.normalize();
+        System.out.println("< " + newVector.getX() + ", " + newVector.getY() + " >");
     }
 }
